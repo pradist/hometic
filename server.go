d43924ed8a23e8b87a3167d0d91d896499d7aa48
Pradist Keawborisut <pradist.k@kbtg.tech>
2020-10-08 12:35:54 +0700

    Add content-type

--- server.go
+++ server.go
@@ -47,6 +47,7 @@ func PairDeviceHandler(device Device) http.HandlerFunc {
 		var p Pair
 		err := json.NewDecoder(r.Body).Decode(&p)
 		if err != nil {
+			w.Header().Set("content-type", "application/json")
 			w.WriteHeader(http.StatusBadRequest)
 			json.NewEncoder(w).Encode(err.Error())
 			return
@@ -55,12 +56,14 @@ func PairDeviceHandler(device Device) http.HandlerFunc {
 
 		err = device.Pair(p)
 		if err != nil {
+			w.Header().Set("content-type", "application/json")
 			w.WriteHeader(http.StatusInternalServerError)
 			json.NewEncoder(w).Encode(err.Error())
 			return
 		}
 
 		fmt.Printf("pair : %#v\n", p)
+		w.Header().Set("content-type", "application/json")
 		w.Write([]byte(`{"status":"active"}`))
 	}
 }
