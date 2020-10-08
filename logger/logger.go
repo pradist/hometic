6843f411c2bc2250d78d7638a602fd64b435ab75
Pradist Keawborisut <pradist.k@kbtg.tech>
2020-10-08 12:14:36 +0700

    Add encapsulate logger

--- logger/logger.go
+++ logger/logger.go
@@ -6,18 +6,22 @@ import (
 	"net/http"
 )
 
+type logKey string
+
+const key logKey = "logger"
+
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		l := zap.NewExample()
 		l = l.With(zap.Namespace("hometicx"), zap.String("I'm", "Gopher"))
 
-		c := context.WithValue(r.Context(), "logger", l)
+		c := context.WithValue(r.Context(), key, l)
 		next.ServeHTTP(w, r.WithContext(c))
 	})
 }
 
 func L(ctx context.Context) *zap.Logger {
-	val := ctx.Value("logger")
+	val := ctx.Value(key)
 
 	if val == nil {
 		return zap.NewExample()
